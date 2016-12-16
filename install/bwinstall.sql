80925828ac3c7f960e685015eb1ec36295e6da80
unknown <>
2016-12-16 22:12:15 +0800

    修复安装SQL失效问题

--- install/bwinstall.sql
+++ install/bwinstall.sql
@@ -59,3 +59,20 @@ CREATE TABLE `bw_baneduser` (
   `nowdate` datetime DEFAULT '1970-01-01 00:00:00',
   PRIMARY KEY (`Id`)
 ); 
+
+CREATE TABLE `bw_admituser` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `oldper` int(11) NOT NULL DEFAULT '1',
+  `newper` int(11) NOT NULL DEFAULT '1',
+  `nowtime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `ifs` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+);
+
+CREATE TABLE `bw_pinfile` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `fileid` int(11) DEFAULT '0',
+  `ifok` int(11) DEFAULT '0',
+  PRIMARY KEY (`Id`)
+);
