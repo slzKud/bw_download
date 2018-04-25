4a71484e3ce03aa376513b5937743bba63ef1f65
slzkud <slzkud@slzlaptop.moe>
2018-04-25 18:57:22 +0800

    更新SQL修改

--- install/bwupdate.sql
+++ install/bwupdate.sql
@@ -3,3 +3,10 @@ CREATE TABLE `bw_filelinks` (
   `LinkDesc` varchar(255) DEFAULT NULL,
   `B64Links` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `bw_chkid` (
+  `chkid` varchar(255) NOT NULL,
+  `chkname` varchar(255) NOT NULL,
+  `motherid` varchar(255) DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `bw_downtable` ADD `chkid` VARCHAR(255) CHARACTER SET utf8  utf8_general_ci NULL AFTER `adddate`;
